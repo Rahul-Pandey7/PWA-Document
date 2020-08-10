9923b1c7d904e3ee9e63331d5c29480a18a6d555
Rahul <rahul.pandey@leanagri.com>
2020-08-10 14:27:03 +0530

    added service worker

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header >
+        Welcome to Revvsales !!
       </header>
+      <p>sample paragraph to be shown in web view and mobile view.</p>
+      <img src="https://designshack.net/wp-content/uploads/placeholder-image.png" />
+      <img src="https://designshack.net/wp-content/uploads/placeholder-image.png" />
+
+      <p className="italic-font"><i><b>Steps need to be viewed </b></i></p>
+      <p>test paragraph 12 If website on small devices behavior like desktop screen then you have to put this meta tag into header before</p>
+      <img src="https://designshack.net/wp-content/uploads/placeholder-image.png" />
+      <img src="https://designshack.net/wp-content/uploads/placeholder-image.png" />
     </div>
   );
 }
